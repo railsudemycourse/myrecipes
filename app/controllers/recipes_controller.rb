e07c2db5341a2bd34e14522be459430762a8947f
Rails Pro Course Udemy <railsudemycourse@gmail.com>
2015-01-27 22:29:37 +0000

    create new and edit forms and partials

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,48 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+  
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def new
+    @recipe = Recipe.new
+  end
+  
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.find(2)
+    
+    if @recipe.save
+      flash[:success] = "Your recipe was created succesfully!"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      #do something
+      flash[:success] = "Your recipe was updated succesfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+    def recipe_params
+      params.require(:recipe).permit(:name, :summary, :description)
+    end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   
   get '/home', to: 'pages#home'
 
+  resources :recipes
+
 end
